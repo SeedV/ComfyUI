d31c40963dba1cb5ed7bd8b7c16585c13ecd4112
zhhezhhe <zhenghe2009@gmail.com>
2023-11-22 17:06:49 +0800

    tail

--- launch.sh
+++ launch.sh
@@ -2,4 +2,4 @@
 #python main.py --listen 0.0.0.0 --disable-xformers --highvram --disable-metadata
 # python main.py --listen 0.0.0.0 --disable-xformers --highvram
 nohup python main.py --listen 0.0.0.0 --disable-xformers --highvram > comfyui_log.log 2>&1 &
-tail -f /dev/null
+tail -f comfyui_log.log
