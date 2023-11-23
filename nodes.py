aa18da1ea6e65bd46569938e5f2311c6a74f8a28
zhhezhhe <zhenghe2009@gmail.com>
2023-11-23 18:38:23 +0800

    gpt prompt refine

--- nodes.py
+++ nodes.py
@@ -62,14 +62,15 @@ class CLIPTextEncode:
 class GptCLIPTextEncode:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"text": ("STRING", {"multiline": True}), "clip": ("CLIP", ),}}
+        return {"required": {"user_text": ("STRING", {"multiline": True}), "default_text": ("STRING", {"multiline": True}), "clip": ("CLIP", ),}}
     RETURN_TYPES = ("CONDITIONING",)
     FUNCTION = "encode"
 
     CATEGORY = "conditioning"
 
-    def encode(self, clip, text):
-        text = self.prompt_refine(text)
+    def encode(self, clip, user_text, default_text):
+        user_text = self.prompt_refine(user_text)
+        text = user_text + ', ' + default_text
         tokens = clip.tokenize(text)
         cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
         return ([[cond, {"pooled_output": pooled}]], )
